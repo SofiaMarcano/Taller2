fc4b0a5dff876b8ebccd7dcb245c9cae68c5ac0a
SofiaMarcano <sofia.marcano@udea.edu.co>
2025-05-12 14:09:03 -0500

    Ver atributos

--- Quiz2.py
+++ Quiz2.py
@@ -2,3 +2,9 @@ import numpy as np
 
 matriz_4d = np.random.rand(20, 30, 20, 100)  
 matriz_3d = matriz_4d[0]
+def see_atributos(m):
+    print("\nAtributos de la matriz:")
+    print(f"Forma: {m.shape}")
+    print(f"Dimensiones: {m.ndim}D")
+    print(f"Tamaño total: {m.size} elementos")
+    print(f"Tipo de datos: {m.dtype}")
